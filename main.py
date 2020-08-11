49895a860b19afa672800097b2c3762a0ebc63a7
BenSandeen <12025856+BenSandeen@users.noreply.github.com>
2020-08-10 23:15:04 -0500

    More small performance improvements
    Also, beginning to figure out OCR for scanning in puzzles

--- main.py
+++ main.py
@@ -22,32 +22,42 @@ class Sudoku:
         self.board = board
         self.recursive_levels = 0
 
-    def solve_puzzle(self, curr_board: List[List[int]], last_modified_cell: Cell = []):
+    # @profile
+    def solve_puzzle(self, curr_board: List[List[int]], last_modified_cell: Cell = Cell(0, 0)):
         self.recursive_levels += 1
         for ii, row in enumerate(curr_board):
+            if ii < last_modified_cell.row:
+                continue
             for jj, cell in enumerate(row):
                 # Used to avoid random num generator picking same nums repeatedly
-                untried_cell_values = list(range(1, self.size + 1))
+                if self.size <= 9:
+                    untried_cell_values = [x for x in range(1, self.size + 1) if x not in row]
+                else:
+                    untried_cell_values = [x for x in range(1, self.size + 1)
+                                           if x not in self.get_nums_in_row(curr_board, Cell(ii, jj)) and
+                                              x not in self.get_nums_in_col(curr_board, Cell(ii, jj)) and
+                                              x not in self.get_nums_in_subsquare(curr_board, Cell(ii, jj))
+                                           ]
 
                 while curr_board[ii][jj] == 0:
                     if untried_cell_values == []:  # If we've tried everything and nothing worked, return and backtrack
                         self.recursive_levels -= 1
                         curr_board[last_modified_cell.row][last_modified_cell.col] = 0  # Reset to zero
                         return
+
                     temp = random.choice(untried_cell_values)
                     untried_cell_values.remove(temp)
+
                     curr_board[ii][jj] = temp
-                    # if self.check_intermediate_board(curr_board, modified_cell=[jj, ii]):
-                    #     self.solve_puzzle(curr_board, last_modified_cell=[ii, jj])
                     if self.check_intermediate_board(curr_board, modified_cell=Cell(ii, jj)):
                         self.solve_puzzle(curr_board, last_modified_cell=Cell(ii, jj))
                     else:
                         curr_board[ii][jj] = 0
 
         if self.check_puzzle(curr_board):
-            self.__repr__()
             return
 
+    # @profile
     def check_puzzle(self, board: List[List[int]]):
         # Add up values in each row as a quick check
         for row in board:
@@ -75,38 +85,58 @@ class Sudoku:
 
         return True
 
-    def check_intermediate_board(self, board: List[List[int]], modified_cell: Cell = []):
+    # @profile
+    def check_intermediate_board(self, board: List[List[int]], modified_cell: Cell):
         """Sort of duplicates some of the code from `check_board()` but that's okay for now.  Checks to make sure there
         only at most one of each number in each row, column, and subsquare
         """
-        for ii, row in enumerate(board):
-            if modified_cell != [] and ii != modified_cell.row:  # Don't check if nothing has changed for this row
-                continue
-            non_zeros_row = [x for x in row if x != 0]
-            if len(set(non_zeros_row)) != len(non_zeros_row):  # Checks for duplicate numbers in row (ignoring zeros)
-                return False
-        for jj, col_idx in enumerate(range(self.size)):  # Don't check if nothing has changed for this col
-            if modified_cell != [] and jj != modified_cell.col:
-                continue
-            non_zeros_col = [x for x in [row[col_idx] for row in board] if x != 0]
-            if len(set(non_zeros_col)) != len(non_zeros_col):
-                return False
-        for subsquare_row_idx in range(self.subsquare_size):
-            for subsquare_col_idx in range(self.subsquare_size):
-                # Make sure the subsquare we're checking is the one that was modified, if the modified cell was provided
-                if modified_cell != [] and \
-                        (modified_cell.col not in range(subsquare_col_idx * self.subsquare_size,
-                                                        subsquare_col_idx * self.subsquare_size + self.subsquare_size) or
-                         modified_cell.row not in range(subsquare_row_idx * self.subsquare_size,
-                                                        subsquare_row_idx * self.subsquare_size + self.subsquare_size)):
-                    continue
+        # for ii, row in enumerate(board):
+        #     if ii != modified_cell.row:  # Don't check if nothing has changed for this row
+        #         continue
+        #     # non_zeros_row = [x for x in row if x != 0]
+        #     non_zeros_row = self.get_nums_in_row(board, modified_cell)
+        #     if len(set(non_zeros_row)) != len(non_zeros_row):  # Checks for duplicate numbers in row (ignoring zeros)
+        #         return False
+        # for jj, col_idx in enumerate(range(self.size)):  # Don't check if nothing has changed for this col
+        #     if jj != modified_cell.col:
+        #         continue
+        #     # non_zeros_col = [x for x in [row[col_idx] for row in board] if x != 0]
+        #     non_zeros_col = self.get_nums_in_col(board, modified_cell)
+        #     if len(set(non_zeros_col)) != len(non_zeros_col):
+        #         return False
+        # for subsquare_row_idx in range(self.subsquare_size):
+        #     if modified_cell.row not in range(subsquare_row_idx * self.subsquare_size,
+        #                                       subsquare_row_idx * self.subsquare_size + self.subsquare_size):
+        #         continue
+        #
+        #     for subsquare_col_idx in range(self.subsquare_size):
+        #         # Make sure the subsquare we're checking is the one that was modified, if the modified cell was provided
+        #         # if (modified_cell.col not in range(subsquare_col_idx * self.subsquare_size,
+        #         #                                    subsquare_col_idx * self.subsquare_size + self.subsquare_size) or
+        #         #         modified_cell.row not in range(subsquare_row_idx * self.subsquare_size,
+        #         #                                        subsquare_row_idx * self.subsquare_size + self.subsquare_size)):
+        #         #     continue
+        #         if modified_cell.col not in range(subsquare_col_idx * self.subsquare_size,
+        #                                           subsquare_col_idx * self.subsquare_size + self.subsquare_size):
+        #             continue
+        #
+        #         subsquare = [row[subsquare_col_idx * self.subsquare_size:subsquare_col_idx * self.subsquare_size + self.subsquare_size]
+        #                      for row in board[subsquare_row_idx * self.subsquare_size: subsquare_row_idx * self.subsquare_size + self.subsquare_size]]
+        #         subsquare_nums = [item for sublist in subsquare for item in sublist]
+        #         non_zeros_subsquare = [x for x in subsquare_nums if x != 0]
+        #         if len(set(non_zeros_subsquare)) != len(non_zeros_subsquare):
+        #             return False
+        non_zeros_row = self.get_nums_in_row(board, modified_cell)
+        if len(set(non_zeros_row)) != len(non_zeros_row):
+            return False
 
-                subsquare = [row[subsquare_col_idx * self.subsquare_size:subsquare_col_idx * self.subsquare_size + self.subsquare_size]
-                             for row in board[subsquare_row_idx * self.subsquare_size: subsquare_row_idx * self.subsquare_size + self.subsquare_size]]
-                subsquare_nums = [item for sublist in subsquare for item in sublist]
-                non_zeros_subsquare = [x for x in subsquare_nums if x != 0]
-                if len(set(non_zeros_subsquare)) != len(non_zeros_subsquare):
-                    return False
+        non_zeros_col = self.get_nums_in_col(board, modified_cell)
+        if len(set(non_zeros_col)) != len(non_zeros_col):
+            return False
+
+        non_zeros_subsquare = self.get_nums_in_subsquare(board, modified_cell)
+        if len(set(non_zeros_subsquare)) != len(non_zeros_subsquare):
+            return False
 
         # If we reach here, then there are no conflicting (i.e., duplicate) numbers in any row, column, or subsquare
         return True
@@ -120,6 +150,53 @@ class Sudoku:
             s += "\n"
         return s
 
+    # @profile
+    def get_nums_in_row(self, curr_board: List[List[int]], cell: Cell):
+        return [x for x in curr_board[cell.row] if x != 0]
+
+    # @profile
+    def get_nums_in_col(self, curr_board: List[List[int]], cell: Cell):
+        return [row[cell.col] for row in curr_board if row[cell.col] != 0]
+
+    # @profile
+    def get_nums_in_subsquare(self, curr_board: List[List[int]], cell: Cell):
+        """Basically copies stuff from `check_intermediate_board()` which is okay for now"""
+        # for subsquare_row_idx in range(self.subsquare_size):
+        #     if cell.row not in range(subsquare_row_idx * self.subsquare_size,
+        #                              subsquare_row_idx * self.subsquare_size + self.subsquare_size):
+        #         continue
+        #     for subsquare_col_idx in range(self.subsquare_size):
+        #         # Make sure the subsquare we're checking is the one that was modified, if the modified cell was provided
+        #         # if (cell.col not in range(subsquare_col_idx * self.subsquare_size,
+        #         #                           subsquare_col_idx * self.subsquare_size + self.subsquare_size) or
+        #         #         cell.row not in range(subsquare_row_idx * self.subsquare_size,
+        #         #                               subsquare_row_idx * self.subsquare_size + self.subsquare_size)):
+        #         #     continue
+        #         if cell.col not in range(subsquare_col_idx * self.subsquare_size,
+        #                                  subsquare_col_idx * self.subsquare_size + self.subsquare_size):
+        #             continue
+        #
+        #         subsquare = [row[subsquare_col_idx * self.subsquare_size:subsquare_col_idx * self.subsquare_size + self.subsquare_size]
+        #                      for row in curr_board[subsquare_row_idx * self.subsquare_size: subsquare_row_idx * self.subsquare_size + self.subsquare_size]]
+        #         subsquare_nums = [item for sublist in subsquare for item in sublist]
+        #         non_zeros_subsquare = [x for x in subsquare_nums if x != 0]
+        #         return non_zeros_subsquare
+        subsquare_row_idx = cell.row // self.subsquare_size
+        subsq_row_start = subsquare_row_idx * self.subsquare_size
+        subsq_row_end = subsq_row_start + self.subsquare_size
+
+        subsquare_col_idx = cell.col // self.subsquare_size
+        subsq_col_start = subsquare_col_idx * self.subsquare_size
+        subsq_col_end = subsq_col_start + self.subsquare_size
+
+        subsquare = [row[subsq_col_start:subsq_col_end] for row in curr_board[subsq_row_start:subsq_row_end]]
+        # subsquare_nums = [item for sublist in subsquare for item in sublist]  # Flatten list of lists into single list
+        # subsquare_non_zeros = [x for x in subsquare_nums if x != 0]
+
+        # Flatten list of lists into single list and ignore zeros
+        subsquare_non_zeros = [item for sublist in subsquare for item in sublist if item != 0]
+        return subsquare_non_zeros
+
 def read_sample_puzzle(file_name):
     puzzle = []
     with open(file_name, 'r') as infile:
@@ -127,10 +204,24 @@ def read_sample_puzzle(file_name):
         for line in reader:
             puzzle.append([int(x) for x in line])
 
+    if len(puzzle) != set([len(row) for row in puzzle]).pop():
+        raise ValueError(f"Input sudoku must be a square!  This is not!")
     return puzzle
 
 
 if __name__ == '__main__':
     s = Sudoku(read_sample_puzzle("sample_puzzle.csv"))
+    s.solve_puzzle(s.board)
+    print(s)
+
+    s = Sudoku(read_sample_puzzle("9x9_tough.csv"))
+    s.solve_puzzle(s.board)
+    print(s)
+
+    # s = Sudoku(read_sample_puzzle("16x16_sample_puzzle.csv"))
+    # s.solve_puzzle(s.board)
+    # print(s)
+
+    s = Sudoku(read_sample_puzzle("16x16_another_puzzle.csv"))
     s.solve_puzzle(s.board)
     print(s)
